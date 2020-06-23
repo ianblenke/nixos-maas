9f67d0fd135e235e3ccc8c14188a99189520c890
Ian Blenke <ian@blenke.com>
2020-06-22 20:26:33 -0400

    Trying things

--- hydra/nixos-maas.nix
+++ hydra/nixos-maas.nix
@@ -7,12 +7,12 @@ with import <nixpkgs> {};
 let
   # The ${...} is for string interpolation
   # The '' quotes are used for multi-line strings
-  simplePackage = pkgs.writeShellScriptBin "whatIsMyIp" ''
-    ${pkgs.curl}/bin/curl https://raw.githubusercontent.com/ianblenke/nixos-maas/master/build-maas-tarball.sh
+  simplePackage = pkgs.writeShellScriptBin "nix-maas" ''
+    ${pkgs.curl}/bin/curl https://raw.githubusercontent.com/ianblenke/nixos-maas/master/build-maas-tarball.sh | ${pkgs.bash}/bin/bash -x
   '';
 in
 stdenv.mkDerivation rec {
   name = "nixos-maas";
 
-  buildInputs = [ simplePackage ];
+  buildInputs = [ simplePackage pkgs.curl pkgs.bash ];
 }
