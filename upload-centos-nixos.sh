b72aa95cda9be57936d2f0d1993df646dd161295
Ian Blenke <ian@blenke.com>
2020-06-24 08:58:52 -0400

    Fix set -xe

--- upload-centos-nixos.sh
+++ upload-centos-nixos.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash -xe
+#!/usr/bin/env bash
+set -xe
 . build-maas-tarball.sh
 maas admin boot-resources create name=custom/nixos-tarball title="NixOS Tarball" architecture=amd64/generic content@=${tarball}
