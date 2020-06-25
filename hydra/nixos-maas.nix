43489a947d12a397609e3511bcd00c4ab1c1557f
Ian Blenke <ian@blenke.com>
2020-06-24 22:20:19 -0400

    Limiting phases

--- hydra/nixos-maas.nix
+++ hydra/nixos-maas.nix
@@ -7,10 +7,6 @@ with import <nixpkgs> {};
 let
   pkgs = import <nixpkgs> { inherit system; };
 
-  buildMaasTarball = pkgs.writeShellScriptBin "build-maas-tarball.sh" ''
-    ${pkgs.curl}/bin/curl https://raw.githubusercontent.com/ianblenke/nixos-maas/master/build-maas-tarball.sh
-  '';
-
   runBuildMaasTarball = stdenv.mkDerivation rec {
     name = "nixos-maas";
   
@@ -21,8 +17,10 @@ let
       buildMaasTarball
     ];
 
+    phases = [ "installPhase" ]
+
     installPhase = '' 
-      ${pkgs.bash}/bin/bash -x build-maas-tarball.sh
+      ${pkgs.curl}/bin/curl https://raw.githubusercontent.com/ianblenke/nixos-maas/master/build-maas-tarball.sh | ${pkgs.bash}/bin/bash -x
     '';
   };
 
